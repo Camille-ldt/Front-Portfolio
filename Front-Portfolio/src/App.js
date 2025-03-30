b1a96e12607c36e638fda00c1a9f9c9d87c47924
Camille <camille.laidet63@gmail.com>
2025-03-30 17:26:42 +0200

    Some update

--- Front-Portfolio/src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/router";
-function App() {
-    return (_jsx("div", { children: _jsx(RouterProvider, { router: router }) }));
-}
-export default App;
