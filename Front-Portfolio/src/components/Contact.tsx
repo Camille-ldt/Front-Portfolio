07806596c5a708a533ebce160abd888c03227c53
Camille <camille.laidet63@gmail.com>
2025-03-31 22:17:33 +0200

    Some update

--- Front-Portfolio/src/components/Contact.tsx
+++ Front-Portfolio/src/components/Contact.tsx
@@ -84,7 +84,7 @@ const Contact: React.FC<ContactProps> = () => {
       console.log("Données du formulaire : ", formData);
 
       const response = await fetch(
-        "https://portfolio-api.onrender.com/api/mailer.php",
+        "https://portfolio-api.onrender.com/mailer.php",
         {
           method: "POST",
           headers: {
